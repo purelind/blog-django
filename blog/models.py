56ddd4f2909134e06a6529792faf14a2b15112ce
purelind <kevinsocial@outlook.com>
2017-08-24 18:35:24 +0800

    __str__ for class in models

--- blog/models.py
+++ blog/models.py
@@ -6,10 +6,15 @@ from django.contrib.auth.models import User
 class Category(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 
 class Tag(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
 
 class Post(models.Model):
     title = models.CharField(max_length=70)
@@ -24,5 +29,6 @@ class Post(models.Model):
     tags = models.ManyToManyField(Tag, blank=True)
 
     author = models.ForeignKey(User)
-    
 
+    def __str__(self):
+        return self.title
