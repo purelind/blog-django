8161fdaa15e864a14c3afaabf0c243e4fb2c1edc
purelind <kevinsocial@outlook.com>
2017-08-25 09:37:04 +0800

    render index.html

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 def index(request):
-    return HttpResponse("Welcome to my blog!")
+    return render(request, 'blog/index.html', context={
+        'title': 'My blog index page',
+        'welcome': 'Welcome to my blog index page.'
+    })
