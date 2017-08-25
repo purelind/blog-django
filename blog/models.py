27444cf55079acf2b08d52780922ce8e5f328754
purelind <kevinsocial@outlook.com>
2017-08-25 10:31:56 +0800

    real blog index view

--- blog/models.py
+++ blog/models.py
@@ -16,6 +16,7 @@ class Tag(models.Model):
     def __str__(self):
         return self.name
 
+
 class Post(models.Model):
     title = models.CharField(max_length=70)
     body = models.TextField()
