7af1510ce7c29ec5229b0119e2638c86f15e094e
purelind <kevinsocial@outlook.com>
2017-08-30 10:21:25 +0800

    category and archive

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,7 @@
 import markdown
 from django.shortcuts import render, get_object_or_404
-from .models import Post
 
-from .models import Post
+from .models import Post, Category
 
 
 # Create your views here.
@@ -23,3 +22,15 @@ def detail(request, pk):
                                   ])
     return render(request, 'blog/detail.html', context={'post': post})
 
+
+def archives(request, year, month):
+    post_list = Post.objects.filter(created_time__year=year,
+                                    created_time__month=month
+                                    ).order_by('-created_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
+
+
+def category(request, pk):
+    cate = get_object_or_404(Category, pk=pk)
+    post_list = Post.objects.filter(category=cate).order_by('-created_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
